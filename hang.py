e68ac514663483db001061b8213bfe976dff31c3
nida maqbool <you@example.com>
2021-05-20 18:43:38 +0500

    1st version

---
+++ hang.py
@@ -0,0 +1,20 @@
+import random
+import turtle 
+import sys
+import time
+
+
+#----------------------------global variables-------------------------------------#
+    
+
+flagW=False
+flagL=False
+flafA=False
+flagWr=False
+gameFlag=True
+wrongCount=0
+hmTC=(0,0)
+hm=turtle.Turtle()
+hm.speed(0)
+state=turtle.Turtle()
+state.ht()
